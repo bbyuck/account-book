0106f54e757117d08f58c6365fc05a2d68842c9b
hyuk <hyuk.kang1@cj.net>
2024-05-24 18:17:11 +0900

    LedgerDetailDto 스펙 수정

--- src/main/java/com/bb/accountbook/domain/ledger/dto/LedgerDetailDto.java
+++ src/main/java/com/bb/accountbook/domain/ledger/dto/LedgerDetailDto.java
@@ -8,12 +8,27 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public abstract class LedgerDetailDto {
     protected Long ledgerId;
     protected LedgerCode ledgerCode;
-    protected LocalDate ledgerDate;
     protected Long ledgerAmount;
     protected String ledgerDescription;
+
+    protected int year;
+    protected int month;
+    protected int day;
+    protected int dayOfWeek;
+
+    protected LedgerDetailDto(Long ledgerId, LedgerCode ledgerCode, LocalDate ledgerDate, Long ledgerAmount, String ledgerDescription) {
+        this.ledgerId = ledgerId;
+        this.ledgerCode = ledgerCode;
+        this.ledgerAmount = ledgerAmount;
+        this.ledgerDescription = ledgerDescription;
+
+        this.year = ledgerDate.getYear();
+        this.month = ledgerDate.getMonthValue();
+        this.day = ledgerDate.getDayOfMonth();
+        this.dayOfWeek = ledgerDate.getDayOfWeek().getValue();
+    }
 }
