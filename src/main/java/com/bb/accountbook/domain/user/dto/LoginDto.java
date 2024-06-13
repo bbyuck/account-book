02cf6979e4137c6863c5b2051a51ca8f525f6836
hyuk <hyuk.kang1@cj.net>
2024-06-13 12:15:31 +0900

    로그인 validation 처리

--- src/main/java/com/bb/accountbook/domain/user/dto/LoginDto.java
+++ src/main/java/com/bb/accountbook/domain/user/dto/LoginDto.java
@@ -2,13 +2,18 @@ package com.bb.accountbook.domain.user.dto;
 
 import com.bb.accountbook.common.constant.ValidationMessage;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LoginDto {
 
     @Email(message = ValidationMessage.INVALID_EMAIL)
+    @NotBlank(message = ValidationMessage.BLANK_EMAIL)
     private String email;
+
+    @NotBlank(message = ValidationMessage.BLANK_PASSWORD)
     private String password;
+
     private boolean autoLogin = false;
 }
