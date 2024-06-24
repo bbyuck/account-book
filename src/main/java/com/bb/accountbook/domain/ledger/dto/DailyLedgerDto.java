3a97a13d137af705313ca603a9623a28d661fe0c
hyuk <k941026h@naver.com>
2024-06-24 18:38:54 +0900

    Monthly API 스펙 변경

--- src/main/java/com/bb/accountbook/domain/ledger/dto/DailyLedgerDto.java
+++ src/main/java/com/bb/accountbook/domain/ledger/dto/DailyLedgerDto.java
@@ -3,16 +3,26 @@ package com.bb.accountbook.domain.ledger.dto;
 import com.bb.accountbook.common.model.codes.LedgerCode;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class DailyLedgerDto {
 
-    private List<LedgerDto> ledgers;
+    private List<LedgerDto> ledgers = new ArrayList<>();
     private Long dailyIncome = 0L;
     private Long dailyExpenditure = 0L;
     private Long dailySave = 0L;
 
+    public void addLedger(LedgerDto ledgerDto) {
+        ledgers.add(ledgerDto);
+
+        switch(ledgerDto.getLedgerCode()) {
+            case E -> this.dailyExpenditure += ledgerDto.getAmount();
+            case I -> this.dailyIncome += ledgerDto.getAmount();
+            case S -> this.dailySave += ledgerDto.getAmount();
+        }
+    }
 
     public void addDailyIncome(Long amount) {
         this.dailyIncome += amount;
