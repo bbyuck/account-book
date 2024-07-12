be46bb101616751cad30ef4848dc2f5293b62f62
hyuk <hyuk.kang1@cj.net>
2024-07-12 17:58:38 +0900

    LedgerStatisticService 개발 타겟 설정

--- src/main/java/com/bb/accountbook/domain/ledger/service/LedgerStatisticService.java
+++ src/main/java/com/bb/accountbook/domain/ledger/service/LedgerStatisticService.java
@@ -5,4 +5,19 @@ package com.bb.accountbook.domain.ledger.service;
  * 가계부 통계 서비스
  */
 public interface LedgerStatisticService {
+
+    /**
+     * 월단위 가계부 코드 별 카테고리 통계
+     * 도넛 그래프
+     */
+
+    /**
+     * 연단위 월 별 가계부 코드 통계
+     * 막대 그래프
+     *
+     * 수입/소득/저축 월 단위 변화 추이 확인
+     */
+
+
+
 }
